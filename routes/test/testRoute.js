efebdfde3e3f3951b3f26baaaad09a22ca149fe0
Biniyamgirma <biniyam0928@gmail.com>
2026-03-14 04:54:17 +0300

    added new environment variable

--- routes/test/testRoute.js
+++ routes/test/testRoute.js
@@ -1,13 +1,15 @@
 const express = require("express");
 const route = express.Router();
 const supabase = require("../../config/supabaseClient");
+require("dotenv").config();
+
 route.get("/", async (req, res) => {
   try {
     const { data, error } = await supabase.from("user").select("*");
     if (error) {
       return res.status(500).json({ error: error.message });
     }
-    res.json(data);
+    res.json({data:data, corsUrl:process.env.CORS_ORIGIN});
   } catch (error) {
     res.status(500).json({ error: "Internal Server Error", data: error });
   }
